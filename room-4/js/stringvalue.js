ca11f94ffef165f80a25443659890fa4070d8afc
Lionel Pham <phamngoctuong1805@gmail.com>
2024-09-28 17:47:55 +0700

    Add Templates

---
+++ room-4/js/stringvalue.js
@@ -0,0 +1,5 @@
+const ZALO_PERMISSION_URL = "https://oauth.zaloapp.com/v4/permission"; // Social API
+const ZALO_ACCESS_TOKEN_URL = "https://oauth.zaloapp.com/v4/access_token"; // Social API
+
+const APP_ID = "1085448574557236173"; // FILL your Application ID here!!!
+const APP_AUTH_URL = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/zalo.html"; // URL to auth.html
